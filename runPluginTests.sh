fe5864b925fe3e4af5f1ed3a6bb91fc90b152000
Arseniy Volynets <vol0ncar@yandex.ru>
2022-03-27 10:05:16 +0300

    upload logs

--- runPluginTests.sh
+++ runPluginTests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update -y
 apt-get install libfreetype6 fontconfig fonts-dejavu -y
-./server/build/utbot server > /dev/null 2>&1 &
+./server/build/utbot server > server-log.txt 2>&1 &
 cd clion-plugin
 ./gradlew test --info --rerun-tasks
 pkill utbot
