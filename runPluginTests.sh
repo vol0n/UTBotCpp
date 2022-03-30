9261177ba3c409f647a587973663b821af445069
Arseniy Volynets <vol0ncar@yandex.ru>
2022-03-30 10:26:50 +0300

    install java after source

--- runPluginTests.sh
+++ runPluginTests.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-sudo apt-get update -y
-apt-get install libfreetype6 fontconfig fonts-dejavu -y
-
-java --version
 # enable needed envs for server
 source docker/building_dependencies/runtime_env.sh
 
-# look at include paths
-cpp -v /dev/null /dev/null
+sudo apt-get update -y
+
+# install font config. Without it java.awt will throw, and ide will exit.
+# Strange that it is not installed by default.
+apt-get install libfreetype6 fontconfig fonts-dejavu -y
+
+#install java zulu 11 distribution
+sudo apt update -y
+sudo apt install dirmngr --install-recommends -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
+sudo apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main' -y
+
+sudo apt update -y
+sudo apt install zulu-11 -y
+
+
+echo "JAVA VERSION"
 java --version
+
+echo "WHICH JAVA"
+which java
+
+
+# look at include paths
+echo "INCLUDE PATHS"
+cpp -v /dev/null /dev/null
 
 set -e
 
@@ -20,6 +39,7 @@ Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 ./server/build/utbot server > server-log.txt 2>&1 &
 
 # look at include paths
+echo "INCLUDE PATHS"
 cpp -v /dev/null /dev/null
 
 cd clion-plugin
